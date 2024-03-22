91b3ef90ffecd0b4a5b1c1dd09c08e3d025eb227
kirtann <94744255+kirtann@users.noreply.github.com>
2024-03-22 18:24:21 +0530

    Question solved

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -31,3 +31,59 @@ exports.getAllComments = (req, res) => {
     });
   });
 };
+
+// Question to get data from three table and sending result
+exports.getThreeTableData = async (req, res) => {
+  const query1 = "SELECT * FROM comments";
+  const query2 = "SELECT * FROM courses";
+  const query3 = "SELECT * FROM instructors";
+  let resu = {};
+
+  const result1 = new Promise((resolve, reject) => {
+    db.query(query1, (err, result) => {
+      if (err) {
+        res.send(err);
+        return;
+      } else {
+        // Was using promises but wrong resolve statement
+        resolve(result);
+      }
+    });
+  });
+
+  const result2 = new Promise((resolve, reject) => {
+    db.query(query2, (err, result) => {
+      if (err) {
+        res.send(err);
+        return;
+      } else {
+        resolve(result);
+      }
+    });
+  });
+
+  const result3 = new Promise((resolve, reject) => {
+    db.query(query3, (err, result) => {
+      if (err) {
+        res.send(err);
+        return;
+      } else {
+        resolve(result);
+      }
+    });
+  });
+
+  // Got stuck at this point as I didn't remember the way to append the data into an object
+  await Promise.all([result1, result2, result3]).then((values) => {
+    resu = {
+      comments: { ...values[0] },
+      courses: { ...values[1] },
+      instructors: { ...values[2] },
+    };
+  });
+
+  res.send({
+    status: "success",
+    data: resu,
+  });
+};
